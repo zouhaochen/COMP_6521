3d11b5ee890cdb3f432fcb14233df8ebe3b5961c
Geer <geer9628@gmail.com>
2022-02-13 10:51:43 -0500

    File path

--- src/assignment1/Main.java
+++ src/assignment1/Main.java
@@ -1,14 +1,15 @@
 package assignment1;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-import static assignment1.RandomNumber.randomListCreation;
 
 public class Main
 {
+
     public static void main(String[] args)
     {
         int[] randomList;
@@ -33,12 +34,15 @@ public class Main
             {
                 case "1":
                     System.out.println();
-                    randomList = randomListCreation();
+                    randomList = RandomNumber.randomListCreation();
                     System.out.println("Random list of integers is created.");
 
                     try
                     {
-                        BufferedWriter out = new BufferedWriter(new FileWriter("input.txt"));
+                        String path = "./src/assignment1/input.txt";
+
+                        File f = new File(path);
+                        BufferedWriter out = new BufferedWriter(new FileWriter(f,true));
                         for(int i = 0; i < randomList.length; i++)
                         {
                             out.write(String.valueOf(randomList[i])+"\r\n");
