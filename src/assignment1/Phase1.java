6d39ece01b65e6716d25f4fed4884796d4701f06
NormalZHC <zouhaochenluke@gmail.com>
2022-02-13 15:51:07 -0500

    update Phase1

--- src/assignment1/Phase1.java
+++ src/assignment1/Phase1.java
@@ -23,6 +23,7 @@ public class Phase1
         return readInput;
 
     }
+
     //Generate sorted Blocks using selection sort
     public static List<List<Integer>> sortBlocks(ArrayList<Integer> readInput){
         System.out.println("Phase 1: ");
@@ -83,26 +84,38 @@ public class Phase1
     public static boolean delAllFile(String path)
     {
         boolean flag = false;
+
         File file = new File(path);
+
         if (!file.exists())
         {
             return flag;
         }
-        if (!file.isDirectory()) {
+
+        if (!file.isDirectory())
+        {
             return flag;
         }
+
         String[] tempList = file.list();
+
         File temp = null;
-        for (int i = 0; i < tempList.length; i++) {
-            if (path.endsWith(File.separator)) {
+
+        for (int i = 0; i < tempList.length; i++)
+        {
+            if (path.endsWith(File.separator))
+            {
                 temp = new File(path + tempList[i]);
-            } else {
-                temp = new File(path + File.separator + tempList[i]);
             }
-            if (temp.isFile()) {
+            else { temp = new File(path + File.separator + tempList[i]); }
+
+            if (temp.isFile())
+            {
                 temp.delete();
             }
-            if (temp.isDirectory()) {
+
+            if (temp.isDirectory())
+            {
                 delAllFile(path + "/" + tempList[i]);
                 flag = true;
             }
