496b811b1e03d73e0eefc06b51a439ac7606f0d0
NormalZHC <zouhaochenluke@gmail.com>
2022-02-13 12:22:50 -0500

    requirement2 finish

---
+++ src/assignment1/DisplayRandom.java
@@ -0,0 +1,46 @@
+package assignment1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class DisplayRandom
+{
+    public static void displayRandom()
+    {
+        String path = "./src/assignment1/input.txt";
+        File file = new File(path);
+
+        FileInputStream fis = null;
+        try
+        {
+            fis = new FileInputStream(file);
+            System.out.println("\nTotal file size to read (in bytes) : " + fis.available());
+            int content;
+
+            while ((content = fis.read()) != -1)
+            {
+                System.out.print((char) content);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (fis != null)
+                {
+                    fis.close();
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
