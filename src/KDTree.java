6f4d1fa7f6b95c09b2149c32d9c0cd4abce139c0
NormalZHC <zouhaochenluke@gmail.com>
2022-03-05 14:21:24 -0500

    KDTree update

--- src/KDTree.java
+++ src/KDTree.java
@@ -1,30 +1,43 @@
-public class KDTree<k> {
+public class KDTree<k>
+{
     public static int k = 2;
-    static class Node{
+
+    public static class Node
+    {
         int point[]= new int[k];
         Node left, right;
     }
-    public static Node newNode(int arr[]){
+
+    public static Node newNode(int arr[])
+    {
         Node temp = new Node();
-        for (int i = 0; i<k; i++){
+
+        for (int i = 0; i<k; i++)
+        {
             temp.point[i] = arr[i];
         }
+
         temp.left = null;
         temp.right = null;
         return temp;
     }
 
-    public static Node insertFunction(Node root, int point[], int depth){
-        if(root == null){
+    public static Node insertFunction(Node root, int point[], int depth)
+    {
+        if(root == null)
+        {
             return newNode(point);
         }
         int currentD = depth % k;
 
-        if(point[currentD] < root.point[currentD]){
+        if(point[currentD] < root.point[currentD])
+        {
             root.left = insertFunction(root.left, point, depth +1);
-        }else{
-            root.right = insertFunction(root.right, point,depth+1);
         }
+        else
+            {
+                root.right = insertFunction(root.right, point,depth+1);
+            }
         return root;
     }
 
@@ -32,25 +45,33 @@ public class KDTree<k> {
         return insertFunction(root, point, 0);
     }
 
-    public static Node minNode(Node x, Node y, Node z, int d){
+    public static Node minNode(Node x, Node y, Node z, int d)
+    {
         Node temp = x;
-        if( y != null && y.point[d] < temp.point[d]){
+        if(y != null && y.point[d] < temp.point[d])
+        {
             temp = y;
         }
-        if(z != null && z.point[d] < temp.point[d]){
+        if(z != null && z.point[d] < temp.point[d])
+        {
             temp = z;
         }
         return temp;
     }
 
-    public static Node findMinFunction(Node root, int d, int depth){
-        if(root == null){
+    public static Node findMinFunction(Node root, int d, int depth)
+    {
+        if(root == null)
+        {
             return null;
         }
 
         int currentD = depth % k;
-        if(currentD == d){
-            if(root.left == null){
+
+        if(currentD == d)
+        {
+            if(root.left == null)
+            {
                 return root;
             }
             return  findMinFunction(root.left, d, depth+1);
@@ -64,48 +85,63 @@ public class KDTree<k> {
         return findMinFunction(root, d, 0);
     }
 
-    public static boolean arePointsSame(int point1[], int point2[]){
-        for(int i = 0; i < k; i++){
-            if(point1[i] != point2[i]){
+    public static boolean arePointsSame(int point1[], int point2[])
+    {
+        for(int i = 0; i < k; i++)
+        {
+            if(point1[i]!= point2[i])
+            {
                 return false;
             }
         }
         return true;
     }
 
-    public static void copyPoint(int p1[], int p2[]){
-        for(int i = 0; i < k; i++){
+    public static void copyPoint(int p1[], int p2[])
+    {
+        for(int i = 0; i < k; i++)
+        {
             p1[i] = p2[i];
         }
     }
 
-    public static Node deleteNodeFunction(Node root, int point[], int depth){
-        if(root == null){
+    public static Node deleteNodeFunction(Node root, int point[], int depth)
+    {
+        if(root == null)
+        {
             return null;
         }
         int currentD = depth % k;
 
-        if(arePointsSame(root.point, point)){
-            if(root.right != null){
+        if(arePointsSame(root.point, point))
+        {
+            if(root.right != null)
+            {
                 Node min = findMin(root.right, currentD);
                 copyPoint(root.point, min.point);
                 root.right = deleteNodeFunction(root.right, min.point,depth+1);
             }
-            else if(root.left != null){
+            else if(root.left != null)
+            {
                 Node min = findMin(root.left, currentD);
                 copyPoint(root.point, min.point);
                 root.right = deleteNodeFunction(root.left, min.point, depth+1);
             }
-            else{
-                return null;
-            }
+            else
+                {
+                    root = null;
+                    return null;
+                }
             return root;
         }
-        if(point[currentD] < root.point[currentD]){
+        if(point[currentD] < root.point[currentD])
+        {
             root.left = deleteNodeFunction(root.left, point, depth+1);
-        }else{
-            root.right = deleteNodeFunction(root.right, point, depth+1);
         }
+        else
+            {
+                root.right = deleteNodeFunction(root.right, point, depth+1);
+            }
         return root;
     }
 
@@ -113,14 +149,19 @@ public class KDTree<k> {
         return deleteNodeFunction(root, point, 0);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         Node root = null;
-        int point[][] = new int[6][k];
-        point = new int[][]{{30, 40}, {5, 25}, {70, 70}, {10, 12}, {50, 30}, {35, 45}};
-        int n = point.length / point[0].length;
-        for(int i =0; i<n; i++){
+        int point[][] = new int[][]{{30, 40}, {5, 25}, {70, 70}, {10, 12}, {50, 30}, {35, 45}};
+
+        //int n = point.length / point[0].length;
+        int n = 6;
+
+        for(int i =0; i<n; i++)
+        {
             root = insert(root, point[i]);
         }
+
         root = deleteNode(root, point[0]);
         System.out.println(" after delete (30,40) : "+ root.point[0]+ root.point[1]);
     }
